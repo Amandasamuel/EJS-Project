6cc860291daeb00d89d078a6c02a5dd39b55cb7c
samuel_markus <markussamuel060@gmail.com>
2022-08-22 19:56:12 +0100

    finnalwork

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.get('/', function(req, res){
     res.send();
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Server started on port 3000");
   });
   

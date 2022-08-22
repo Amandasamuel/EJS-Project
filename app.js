6737e9d297e86cfc3a0c6b6981793f756e9216b4
samuel_markus <markussamuel060@gmail.com>
2022-08-22 14:09:58 +0100

    was able to add content from the compose page

--- app.js
+++ app.js
@@ -13,11 +13,13 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+let posts = [];
+
 
 
 app.get('/', function(req, res){
-    res.render('home', {StartingContent:homeStartingContent });
-    
+    res.render('home', {StartingContent:homeStartingContent, posts:posts });
+   
 });
 
 app.get('/about', function(req, res){
@@ -36,7 +38,12 @@ app.get('/compose', function(req, res){
 });
 
 app.post("/compose", function(req, res){
-    console.log(req.body.postBody);
+    const post ={
+        title:req.body.postTitle,
+        content:req.body.postBody
+    }
+    posts.push(post);
+   res.redirect("/")
 });
  
 app.get('/', function(req, res){
